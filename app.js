bc2ed80ceceb80531ba693c089c5262be694d226
Anand <mailtoanand06@gmail.com>
2019-02-18 22:16:39 +0530

    Name Calculator example with angularJS

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-(function (){
-  angular.module("FirstApp",[])
+(function(){
+  'use strict';
 
-  .controller("FirstController", function($scope){
-    $scope.name = "Evans";
+  angular.module('NameCalculator',[])
 
-    $scope.sayName = function () {
-       return "Hello Evans!!";
+  .controller('NameCalculatorController', function($scope){
+    $scope.name = "";
+    $scope.totalValue = 0;
+
+    $scope.displayNumeric = function(){
+      var totalNameValue = calculateNumeric($scope.name);
+      $scope.totalValue = totalNameValue;
     };
+
+    function calculateNumeric (string){
+      var totalStringValue = 0;
+      for (var i = 0; i < string.length; i++) {
+        totalStringValue += string.charCodeAt(i);
+      }
+      return totalStringValue
+    };
+
   });
 })();
